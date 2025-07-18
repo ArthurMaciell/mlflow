db1c2e8eeea036fa7a25cb0ed34f00505664a017
Arthur Maciel <arthur6325@gmail.com>
2025-07-18 14:32:46 -0300

    model trainer adicionado

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from mlflowproj import logger
 from mlflowproj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlflowproj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlflowproj.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlflowproj.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -20,6 +23,29 @@ STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
     data_ingestion = DataValidationTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_ingestion = ModelTrainerTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
